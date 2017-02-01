efbeac53ae153b2112abc8afefa3d28aa162a0a8
Hardy <haxxorhardy@gmail.com>
2017-02-01 14:09:55 +0100

    comments and errorhandling

--- Starter/Module1.vb
+++ Starter/Module1.vb
@@ -1,35 +1,43 @@
 ﻿Module Module1
 
     Sub Main()
+        Try
+            Dim myPath = My.Application.Info.DirectoryPath                  'get current directory
+            Dim fname As String = Foo(myPath)                               'call Function Foo to get filename of xClient
 
-        Dim myPath = My.Application.Info.DirectoryPath
-        Dim fname As String = Foo(myPath)
+            Console.WriteLine(fname)                                        'testing not visible
 
-        Console.WriteLine(fname)
+            Dim procStartInfo As New ProcessStartInfo                       'Dims to start the xClient as admin
+            Dim procExecuting As New Process
 
-        Dim procStartInfo As New ProcessStartInfo
-        Dim procExecuting As New Process
+            With procStartInfo
+                .UseShellExecute = True
+                .FileName = fname
+                .Verb = "runas"                                              'add this to prompt for adminrights
+                .WorkingDirectory = myPath
+            End With
 
-        With procStartInfo
-            .UseShellExecute = True
-            .FileName = fname
-            .Verb = "runas" 'add this to prompt for adminrights
-            .WorkingDirectory = myPath
-        End With
-
-        procExecuting = Process.Start(procStartInfo)
-
+            procExecuting = Process.Start(procStartInfo)                     'here we go and start the Process
+        Catch ex As Exception
+            Console.WriteLine(ex.Message, Environment.NewLine)               'Errorhandling for user debugging
+            Console.ReadLine()
+        End Try
     End Sub
 
 
     Private Function Foo(ByVal directory As String)
-        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
-            Dim fileExtension As String = IO.Path.GetExtension(filename)
-            If fileExtension = ".exe" Then
-                Return filename.ToString
-            End If
-        Next
-        Return Nothing
+        Try
+            For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)        'search for all files in directory
+                Dim fileExtension As String = IO.Path.GetExtension(filename)                                            'look for the extension (.exe .txt .bat etc.)
+                If fileExtension = ".exe" Then                                                                          'we only need .exe
+                    Return filename.ToString                                                                            'return the filename :)
+                End If
+            Next
+            Return Nothing
+        Catch ex As Exception
+            Console.WriteLine(ex.Message, Environment.NewLine)                                                          'Errorhandling for user debugging
+            Console.ReadLine()
+            Return Nothing
+        End Try
     End Function
-
 End Module
