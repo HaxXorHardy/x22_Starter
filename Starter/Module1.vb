2e75f2b54a87886323fb59c7f6740218972061ac
Hardy <haxxorhardy@gmail.com>
2017-02-01 14:00:39 +0100

    Projektdateien hinzufügen.

---
+++ Starter/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim myPath = My.Application.Info.DirectoryPath
+        Dim fname As String = Foo(myPath)
+
+        Console.WriteLine(fname)
+
+        Dim procStartInfo As New ProcessStartInfo
+        Dim procExecuting As New Process
+
+        With procStartInfo
+            .UseShellExecute = True
+            .FileName = fname
+            .Verb = "runas" 'add this to prompt for adminrights
+            .WorkingDirectory = myPath
+        End With
+
+        procExecuting = Process.Start(procStartInfo)
+
+    End Sub
+
+
+    Private Function Foo(ByVal directory As String)
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+            Dim fileExtension As String = IO.Path.GetExtension(filename)
+            If fileExtension = ".exe" Then
+                Return filename.ToString
+            End If
+        Next
+        Return Nothing
+    End Function
+
+End Module
